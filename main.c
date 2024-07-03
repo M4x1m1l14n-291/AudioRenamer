6597d9263168b64474ce13f6b6e1319aa1fff04c
Maximilian Riepl <Maximilian291@gmail.com>
2024-07-03 13:15:28 +0200

    threads

--- main.c
+++ main.c
@@ -8,10 +8,8 @@ float volume = 0.5;
 void *playSound(void *path)
 {
     printf("start playing: %s\n", (char *)path);
-
     char command[256];
-    snprintf(command, sizeof(command), "ffplay -v 0 -nodisp -autoexit -af \"volume=%f\" ./%s", volume, (char *)path);
-
+    snprintf(command, sizeof(command), "ffplay -v 0 -nodisp -autoexit -af \"volume=%f\" \"./%s\"", volume, (char *)path);
     system(command);
 }
 
@@ -19,13 +17,25 @@ int main(int argc, char const *argv[])
 {
     printf("please enter volume (0.0 -> 1.0)\n");
     scanf("%f", &volume);
-    if (volume > 1.0)
+
+    if (argc == 1)
     {
-        volume = 1.0;
+        printf("WIP :scan current directory\n");
+        return 0;
+    }
+    else if (argc == 2)
+    {
+        char input;
+        pthread_t thread_id;
+
+        pthread_create(&thread_id, NULL, playSound, argv[1]);
+        pthread_join(thread_id, NULL);
+    }
+    else if (argc == 3 && !strcmp(argv[1], "--directory"))
+    {
+        printf("WIP: scan directory\n");
+        return 0;
     }
 
-    pthread_t thread_id;
-    pthread_create(&thread_id, NULL, playSound, "audio.mp3");
-    pthread_join(thread_id, NULL);
     return 0;
 }
