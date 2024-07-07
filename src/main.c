ae4a19260f8224f85fa79b00b0407a086fdbcb9f
Maximilian Riepl <Maximilian291@gmail.com>
2024-07-07 10:23:27 +0200

    removed unused header files

--- src/main.c
+++ src/main.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <utils.h>
 #include <signal.h>
 #include <dirent.h>
-
-#include <ctype.h>
 
 #include "main.h"
 #include "linkedList.h"
